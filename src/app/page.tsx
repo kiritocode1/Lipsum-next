f5757087d1a3a269a5b660769a706863568dd88b
Riyadh Muradi <theahtic@gmail.com>
2024-06-17 21:27:15 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="container relative">
       <div className="mx-auto flex flex-col items-center gap-2 py-8">
-        {/* Components here. */}
+        {/* Components here. a */}
       </div>
     </main>
   );
